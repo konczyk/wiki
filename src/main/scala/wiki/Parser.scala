7b75c67969532b6650b481e3dbf442c964ffff60
Daniel Kończyk <daniel@konczyk.net>
2017-05-24 13:36:58 +0200

    Parse page id too

--- src/main/scala/wiki/Parser.scala
+++ src/main/scala/wiki/Parser.scala
@@ -3,43 +3,39 @@ package wiki
 import scala.io.Source
 import scala.xml.pull._
 
-case class Page(title: String, text: String)
+case class Page(id: Int, title: String, text: String)
 
 class Parser(src: Source) extends Iterator[Page] {
 
   private val reader = new XMLEventReader(src)
 
-  def hasNext: Boolean = forward(reader)
-  def next: Page = Page(getTitle(reader), getText(reader))
+  def hasNext: Boolean = reader.exists {
+    case EvElemStart(_, "page", _, _) => true
+    case _ => false
+  }
 
-  private def forward(reader: Iterator[XMLEvent]) =
-    reader.exists {
-      case EvElemStart(_, "page", _, _) => true
-      case _ => false
-    }
+  def next: Page = {
+    val title = getElement("title")
+    val id = getElement("id")
+    val text = getElement("text")
+    Page(id.toInt, title, text)
+  }
 
-  private def getTitle(reader: Iterator[XMLEvent]) =
-    getElement(reader, "title")
-
-  private def getText(reader: Iterator[XMLEvent]) =
-    getElement(reader, "text")
-
-  private def getElement(reader: Iterator[XMLEvent], element: String) = {
+  private def getElement(element: String) = {
     import scala.collection.mutable
-    def go(reader: Iterator[XMLEvent], acc: Option[mutable.StringBuilder]): String =
-      (reader.next, acc) match {
-        case (EvElemStart(_, elem, _, _), _) if elem == element =>
-          go(reader, Some(new mutable.StringBuilder))
-        case (EvElemEnd(_, elem), Some(buf)) if elem == element =>
-          buf.toString
-        case (EvText(text), Some(buf)) =>
-          go(reader, Some(buf ++= text))
-        case (EvEntityRef(entity), Some(buf)) =>
-          go(reader, Some(buf ++= s"&$entity;"))
-        case _ =>
-          go(reader, acc)
-      }
-    go(reader, None)
+    def go(acc: Option[mutable.StringBuilder]): String = (reader.next, acc) match {
+      case (EvElemStart(_, elem, _, _), _) if elem == element =>
+        go(Some(new mutable.StringBuilder))
+      case (EvElemEnd(_, elem), Some(buf)) if elem == element =>
+        buf.toString
+      case (EvText(text), Some(buf)) =>
+        go(Some(buf ++= text))
+      case (EvEntityRef(entity), Some(buf)) =>
+        go(Some(buf ++= s"&$entity;"))
+      case _ =>
+        go(acc)
+    }
+    go(None)
   }
 
 }
--- src/test/scala/wiki/ParserTest.scala
+++ src/test/scala/wiki/ParserTest.scala
@@ -14,31 +14,31 @@ class ParserTest extends FunSuite {
 
   test("single page is extracted") {
     val src = scala.io.Source.fromString(
-      "<xml><x>vae</x><page><title>x</title><id/><text>y</text></page><y/></xml>")
+      "<xml><x>vae</x><page><title>x</title><id>5</id><text>y</text></page><y/></xml>")
     val parser = new Parser(src)
     assert(parser.hasNext === true)
-    assert(parser.next === Page("x", "y"))
+    assert(parser.next === Page(5, "x", "y"))
     assert(parser.hasNext === false)
   }
 
   test("two pages are extracted") {
     val src = scala.io.Source.fromString(
-      """<xml><x>vae</x><page><title>x</title><id/><text>y</text></page><y/>
-        |<page><title>w</title><text>z</text></page></xml>""".stripMargin)
+      """<xml><x>vae</x><page><title>x</title><id>8</id><text>y</text></page><y/>
+        |<page><title>w</title><id>9</id><text>z</text></page></xml>""".stripMargin)
     val parser = new Parser(src)
     assert(parser.hasNext === true)
-    assert(parser.next === Page("x", "y"))
+    assert(parser.next === Page(8, "x", "y"))
     assert(parser.hasNext === true)
-    assert(parser.next === Page("w", "z"))
+    assert(parser.next === Page(9, "w", "z"))
     assert(parser.hasNext === false)
   }
 
   test("html entities are included") {
     val src = scala.io.Source.fromString(
-      "<xml><page><title>x</title><text>y &amp;nbsp; &lt;code/&gt;</text></page></xml>")
+      "<xml><page><title>x</title><id>9</id><text>y &amp;nbsp; &lt;code/&gt;</text></page></xml>")
     val parser = new Parser(src)
     assert(parser.hasNext === true)
-    assert(parser.next === Page("x", "y &amp;nbsp; &lt;code/&gt;"))
+    assert(parser.next === Page(9, "x", "y &amp;nbsp; &lt;code/&gt;"))
   }
 
 }
